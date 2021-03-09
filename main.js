a03d9c5555a41aba6361657a80c88feb44268b34
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-03-09 23:17:33 +0300

    fix: main js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-(function() {
+// (function() {
   
     let buttonRain = document.getElementById('rain'),
-        buttonSun = document.getElementById('sum'),
+        buttonSun = document.getElementById('sum'), 
         video = document.getElementById('video'),
          audio = document.getElementById('audio'),
          pause = document.getElementById('pause'),
          play = document.getElementById('play'),
         button120s = document.getElementById ('120s'),
         button300s = document.getElementById ('300s'),
-        button1000s = document.getElementById ('1000s');
+        button1000s = document.getElementById ('1000s'),
+        fakeDuration = 600;
    
         buttonRain.onclick = function () {
             video.src = "/video/rain.mp4";
@@ -38,11 +39,30 @@
             pause.style.display = "unset";
         }
 
-        button120s.onclick = function() {
-            if (video.current.Time() = 120) {
-                video.pause()
-                audio.pause()
-            }
+        video.ontimeupdate = progressUpdate;
+        function progressUpdate() {
+        let videoTime = video.currentTime;
+        let videoZ = Math.floor(videoTime);
         }
-    
-})();
+
+
+        // video.ontimeupdate = function() {
+        //     let currentTime = video.currentTime;
+        //     let elapsed = fakeDuration - currentTime;
+        //     // console.log(elapsed);
+
+        //     let seconds = Math.floor(elapsed % 60);
+        //     // console.log(seconds);
+        //     let minutes = Math.floor(elapsed / 60);
+        //     // timeDisplay.textContent = `${minutes}:${seconds}`;
+        //     console.log(seconds);       
+           
+        //     // outline.style.strokeDashoffset = progress;
+          
+        //     if (currentTime >= fakeDuration) {
+        //       song.pause();
+        //       song.currentTime = 0;
+        //       play.src = "./svg/play.svg";
+        //       video.pause();
+        //     }
+        //   };
