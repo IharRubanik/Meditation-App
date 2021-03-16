a9ae395d925f3fa96892e7378d5d4674a996bb10
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-03-16 23:16:13 +0300

    fix: time js

--- main.js
+++ main.js
@@ -1,49 +1,134 @@
 // (function() {
   
-    let buttonRain = document.getElementById('rain'),
-        buttonSun = document.getElementById('sum'), 
-        video = document.getElementById('video'),
-         audio = document.getElementById('audio'),
-         pause = document.getElementById('pause'),
-         play = document.getElementById('play'),
-        button120s = document.getElementById ('120s'),
-        button300s = document.getElementById ('300s'),
-        button1000s = document.getElementById ('1000s'),
-        fakeDuration = 600;
+    // let buttonRain = document.getElementById('rain'),
+    //     buttonSun = document.getElementById('sum'), 
+    //     video = document.getElementById('video'),
+    //      audio = document.getElementById('audio'),
+    //      pause = document.getElementById('pause'),
+    //      play = document.getElementById('play'),
+    //     button120s = document.getElementById ('120s'),
+    //     button300s = document.getElementById ('300s'),
+    //     button1000s = document.getElementById ('1000s'),
+    //     fakeDuration = 600;
    
-        buttonRain.onclick = function () {
-            video.src = "/video/rain.mp4";
-            audio.src = "/audio/rain.mp3";
-            play.style.display = "none";
-            pause.style.display = "unset"; 
-        } 
+    //     buttonRain.onclick = function () {
+    //         video.src = "/video/rain.mp4";
+    //         audio.src = "/audio/rain.mp3";
+    //         play.style.display = "none";
+    //         pause.style.display = "unset"; 
+    //     } 
 
-        buttonSun.onclick = function () {
-            video.src = "/video/sea.mp4";
-            audio.src = "/audio/sea.mp3";
-            play.style.display = "none";
-            pause.style.display = "unset";
-        }
+    //     buttonSun.onclick = function () {
+    //         video.src = "/video/sea.mp4";
+    //         audio.src = "/audio/sea.mp3";
+    //         play.style.display = "none";
+    //         pause.style.display = "unset";
+    //     }
         
-        pause.onclick = function () {
-            video.pause();
-            audio.pause();
-            play.style.display = "unset";
-            pause.style.display = "none";
-        }
+    //     pause.onclick = function () {
+    //         video.pause();
+    //         audio.pause();
+    //         play.style.display = "unset";
+    //         pause.style.display = "none";
+    //     }
 
-        play.onclick = function() {
-            video.play();
-            audio.play();
-            play.style.display = "none";
-            pause.style.display = "unset";
-        }
+    //     play.onclick = function() {
+    //         video.play();
+    //         audio.play();
+    //         play.style.display = "none";
+    //         pause.style.display = "unset";
+    //     }
 
-        video.ontimeupdate = progressUpdate;
-        function progressUpdate() {
-        let videoTime = video.currentTime;
-        let videoZ = Math.floor(videoTime);
+    //     video.ontimeupdate = progressUpdate;
+    //     function progressUpdate() {
+    //     let videoTime = video.currentTime;
+    //     let videoZ = Math.floor(videoTime);
+    //     }
+
+
+
+
+
+    (function () {
+
+            let buttonRain = document.getElementById('rain'),
+                buttonSun = document.getElementById('sum'),
+                video = document.getElementById('video'),
+                audio = document.getElementById('audio'),
+                pause = document.getElementById('pause'),
+                play = document.getElementById('play'),
+                button120s = document.getElementById('120s'),
+                button300s = document.getElementById('300s'),
+                button1000s = document.getElementById('1000s'),
+                interv,
+                time
+                // timeOut = document.getElementById('time')
+    
+    
+            buttonRain.onclick = function () {
+                if (video.src !== 'video/rain.mp4') {
+                    time = 0
+                }
+                clearInterval(interv)
+                interv = setInterval(() => {
+                    time++
+                    console.log(time)
+    
+                }, 1000)
+                video.src = 'video/rain.mp4'
+                audio.src = 'audio/rain.mp3'
+                play.style.display = 'none'
+                pause.style.display = 'unset'
+            }
+    
+            buttonSun.onclick = function () {
+                if (video.src !== 'video/sea.mp4') {
+                    time = 0
+                }
+                clearInterval(interv)
+                interv = setInterval(() => {
+                    timeOut = time++
+                    console.log(time)
+    
+                }, 1000)
+                video.src = 'video/sea.mp4'
+                audio.src = 'audio/sea.mp3'
+                play.style.display = 'none'
+                pause.style.display = 'unset'
+            }
+    
+            pause.onclick = function () {
+                clearInterval(interv)
+                video.pause()
+                audio.pause()
+                play.style.display = 'unset'
+                pause.style.display = 'none'
+            }
+    
+            play.onclick = function () {
+                if (video.src) {
+                    interv = setInterval(() => {
+                        time++
+                        console.log(time)
+    
+                    }, 1000)
+                    video.play()
+                    audio.play()
+                    play.style.display = 'none'
+                    pause.style.display = 'unset'
+                }
+            }
+    
+            // button120s.onclick = function () {
+            //     if (video.current.Time() = 120) {
+            //         video.pause()
+            //         audio.pause()
+            //     }
+            // }
+    
         }
+    )()
+
 
 
         // video.ontimeupdate = function() {
